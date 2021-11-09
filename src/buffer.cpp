98a7ae4f4b3185ee9e0ee377e986670020cf8a9d
Alexander Beers <abeers@snares-06.cs.wisc.edu>
2021-11-08 23:07:00 -0600

    Implemented advanced clock, partial implementation of allocbuf&allocpage

--- src/buffer.cpp
+++ src/buffer.cpp
@@ -38,15 +38,53 @@ BufMgr::BufMgr(std::uint32_t bufs)
   clockHand = bufs - 1;
 }
 
-void BufMgr::advanceClock() {}
+void BufMgr::advanceClock() {
+  //increments clockHand to next position
+  if(clockHand == numBufs-1){
+      clockHand = 0;
+  }else{
+      clockHand++;
+  }
+}
 
-void BufMgr::allocBuf(FrameId& frame) {}
+void BufMgr::allocBuf(FrameId& frame) {
+   //Go through all frames twice(in the case that no frame has ref=0 on first cycle)
+   for(unsigned int i = 0; i < numBufs*2; i++){
+      //Case if frame is not valid
+      if(bufDescTable[clockHand].valid == false){
+	 frame = clockHand;
+	 bufDescTable[clockHand].pinCnt++;
+	 bufDescTable[clockHand].valid = true;
+	 return;
+      //Case if a frame is available to pick
+      }else if(bufDescTable[clockHand].refbit == false && bufDescTable[clockHand].pinCnt == 0){
+         frame = clockHand;
+	 bufDescTable[clockHand].pinCnt++;
+	 return;
+      //case if ref needs to be reset
+      }else if(bufDescTable[clockHand].refbit == true){
+         bufDescTable[clockHand].refbit = false;
+	 advanceClock();
+      }else{ //march forwards
+         advanceClock();
+      }
+   }
+   //TODO: throw exception here(no frame found)
+}
 
 void BufMgr::readPage(File& file, const PageId pageNo, Page*& page) {}
 
 void BufMgr::unPinPage(File& file, const PageId pageNo, const bool dirty) {}
 
-void BufMgr::allocPage(File& file, PageId& pageNo, Page*& page) {}
+void BufMgr::allocPage(File& file, PageId& pageNo, Page*& page) {
+   Page p  = file.allocatePage();
+   FrameId fId = -1;
+   allocBuf(fId); //allocate the frame
+   std::cout << fId << std::flush; //delete this
+   //TODO:
+   //insert to hash
+   //Call set
+}
 
 void BufMgr::flushFile(File& file) {}
 
--- src/main.cpp
+++ src/main.cpp
@@ -157,7 +157,7 @@ void testBufMgr() {
 }
 
 void test1(File &file1) {
-  // Allocating pages in a file...
+  // Allocating pages in a file.
   for (i = 0; i < num; i++) {
     bufMgr->allocPage(file1, pid[i], page);
     sprintf(tmpbuf, "test.1 Page %u %7.1f", pid[i], (float)pid[i]);
